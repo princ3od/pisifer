0665b47136dff5dfe40939a7a1656c48b49e1b76
Trong <princ3od@gmail.com>
2021-12-20 00:27:58 +0700

    [feat]: add backend API

---
+++ README.md
@@ -0,0 +1,20 @@
+# Topic Classification API
+
+A simple API to classify topic of text.
+
+
+## Deployment
+
+Docker must be installed  
+To deploy this project run
+```bash
+docker-compose build 
+docker-compose up -d
+```
+
+
+## Authors
+
+- [Dương Bình Trọng](https://www.github.com/princ3od)
+- [Phạm Đức Hoàng](https://github.com/PRID021)  
+- [Trần Đặng Hoàng Anh](https://github.com/TranDangHoangAnh)
---
+++ server/inference.py
@@ -0,0 +1,41 @@
+import re
+from gensim.models.doc2vec import Doc2Vec
+from nltk.tokenize import word_tokenize
+from gensim.parsing.preprocessing import STOPWORDS
+from datetime import date
+
+
+def load_model(filename='d2v.model'):
+    global model
+    model = Doc2Vec.load(filename)
+    return model
+
+
+def rm_stopwords(tokenized_doc):
+    tok_without_sw = []
+    for txt_tokens in tokenized_doc:
+        tok_without_sw = [
+            word for word in tokenized_doc if not word.lower() in STOPWORDS]
+    return tok_without_sw
+
+
+def infer(text, top=3, remove_num=False):
+    if top > 7:
+        top = 7
+    if top < 1:
+        top = 1
+    text = re.sub(r'[^\w\s]', '', text)
+    if remove_num:
+        text = re.sub(r'\d+', '', text)
+    processed_text = word_tokenize(text.lower())
+    processed_text = rm_stopwords(processed_text)
+    raw_result = model.docvecs.most_similar(
+        positive=[model.infer_vector(processed_text)], topn=top)
+    result = []
+    for i in range(top):
+        result.append({raw_result[i][0]: round(raw_result[i][1], 4)})
+    return {
+        'output': result,
+        'processed_text': ' '.join(processed_text),
+        'time': date.today(),
+    }
