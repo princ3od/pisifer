aed881e11d4cc59369e778451dfddb1fcc62dd85
Trong <princ3od@gmail.com>
2021-12-20 00:27:58 +0700

    [feat]: add backend API

---
+++ server/app.py
@@ -0,0 +1,24 @@
+from logging import log
+import os
+from flask import Flask, json, request
+from inference import load_model, infer
+import nltk
+nltk.download('punkt')
+
+app = Flask(__name__)
+
+load_model("d2v.model")
+
+
+@app.route('/topic-classification', methods=['POST'])
+def topic_classification():
+    json_data = request.get_json()
+    text = json_data['text']
+    top = json_data['top'] if 'top' in json_data else 3
+    remove_num = json_data['remove_num'] if 'remove_num' in json_data else False
+    result = infer(text, top, remove_num)
+    return json.jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
