294b05f9f7e21f991e91f8dcdfb9b09d465291f4
Trong <princ3od@gmail.com>
2022-01-03 12:51:25 +0700

    [fix]: add cors

--- server/app.py
+++ server/app.py
@@ -1,21 +1,23 @@
 from logging import log
 import os
 from flask import Flask, json, request
+from flask_cors import CORS
 from inference import load_model, infer
 import nltk
-nltk.download('punkt')
+
+nltk.download("punkt")
 
 app = Flask(__name__)
-
+CORS(app)
 load_model("d2v.model")
 
 
-@app.route('/topic-classification', methods=['POST'])
+@app.route("/topic-classification", methods=["POST"])
 def topic_classification():
     json_data = request.get_json()
-    text = json_data['text']
-    top = json_data['top'] if 'top' in json_data else 3
-    remove_num = json_data['remove_num'] if 'remove_num' in json_data else False
+    text = json_data["text"]
+    top = json_data["top"] if "top" in json_data else 3
+    remove_num = json_data["remove_num"] if "remove_num" in json_data else False
     result = infer(text, top, remove_num)
     return json.jsonify(result)
 
